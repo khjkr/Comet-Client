160bd4d2487b35241ec4f0bfc8e209f70733f8a1
Hyunjun Kim <hyunjun5216@naver.com>
2021-02-13 16:30:52 +0900

    initial commit

---
+++ public/Main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: false,
+    titleBarStyle: 'hidden'
+  })
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+  })
+  
+  win.loadURL(startUrl);
+
+}
+
+app.on('ready', createWindow)
